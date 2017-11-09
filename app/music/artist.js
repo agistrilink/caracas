9d06280d01560192d18c5e927e0c3c28280afa6c
Harrold Korte <agistrilink@outlook.com>
2017-11-09 22:41:25 +0100

    artist sync

--- app/music/artist.js
+++ app/music/artist.js
@@ -36,13 +36,22 @@ class Artist extends Directory {
         });
 
         const name = this.getAlbumNames().find(name => {
-            return options.strict ? name === title : strpos(title, name) === 0;
+            return options.strict ? name === title : name.indexOf(title) === 0;
         });
 
         return !!name ? new Album({fullPath: path.join(this.fullPath, name)}) : undefined;
     }
 
-    addAlbum(name){
+    copyAlbum(from){
+        const fromAlbum = new Album(from),
+            isFlacToMp3 = this.encoding === 'mp3' && Encoding.isFlac(fromAlbum.encoding);
+
+        if (isFlacToMp3){
+
+        }
+        else {
+            // deep copy of fromAlbum.fullPath to this.fullPath
+        }
         return this.createDir(name);
     }
 
@@ -54,23 +63,46 @@ class Artist extends Directory {
         return this.getSubDirectoriesBaseNames(options);
     }
 
-    static sync(master, slave){/*
+    static sync(master, slave){
+        // probably master album renamed
         slave.getAlbumNames()
             .filter(name => {
-                return !master.hasAlbum(name);
+                return !master.hasAlbum(new Album(name).title);
             })
-            .forEach(artistName => {
-                slave.removeAlbum(artistName);
+            .forEach(name => {
+                slave.removeAlbum(name);
             });
 
-        master.getAlbumNames()
-            .forEach(artistName => {
-                Album.sync(
-                    master.getAlbum(artistName),
-                    slave.hasAlbum(artistName) ? slave.getAlbum(artistName) : slave.addAlbum(artistName)
-                );
-            }); */
+        // encoding differences
+        slave.getAlbumNames({force: true})
+            .filter(name => {
+                return !master.hasAlbum(name, {strict: true});
+            })
+            .forEach(name => {
+                const masterAlbumEncoding = master.getAlbum(Album.asTitle(name)).encoding,
+                    slaveAlbum = slave.getAlbum(name, {strict: true}),
+                    slaveAlbumEncoding = slaveAlbum.encoding,
+                    compare = Encoding.compare(masterAlbumEncoding, slaveAlbumEncoding);
 
+                // master album encoding lower or equal than slave's
+                if (Encoding.compare(masterAlbumEncoding, slaveAlbumEncoding) <= 0){
+                    return;
+                }
+
+                // master album encoding bigger now
+
+                if (slave.encoding === 'any' || slaveAlbumEncoding.valueOf() !== Encoding.KBS320){
+                    slave.removeAlbum(name);
+                }
+            });
+
+        master.getArtistNames()
+            .filter(name => {
+                return !slave.hasAlbum(name);
+            })
+            .forEach(name => {
+                slave.copyAlbum(name);
+            });
     }
 
 };
