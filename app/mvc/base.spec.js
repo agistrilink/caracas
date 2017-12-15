392c916fa8ba260a3a0eafc0885ce62d2737e5cf
Harrold Korte <harrold.korte@idealo.de>
2017-12-15 15:18:55 +0100

    with karma (to be removed) and jasmine

--- app/mvc/base.spec.js
+++ app/mvc/base.spec.js
@@ -2,7 +2,7 @@
 
 const _ = require('./kraftaverk');
 
-describe("Base suite", function() {
+describe("Kraftaverk suite", function() {
     it("contains spec with an expectation", function() {
         expect(true).toBe(true);
     });
